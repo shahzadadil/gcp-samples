76825fb87bcab4581276fb4e5331656acef95439
Shahzad <shahzad.adil@e-zest.in>
2022-08-22 14:06:22 +0530

    reorganise

---
+++ logging/net48/GCPLogging/Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿namespace Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Google.Apis.Auth.OAuth2;
+    using Google.Cloud.Storage.V1;
+
+    using log4net;
+
+    public class HomeController : Controller
+    {
+        private ILog _Logger = LogManager.GetLogger(typeof(HomeController));
+
+        public ActionResult Index()
+        {
+           
+            _Logger.Info("Executing Index method");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ logging/net48/GCPLogging/Web/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿namespace Web
+{
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using log4net;
+    using log4net.Config;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            XmlConfigurator.Configure(
+                LogManager.GetRepository(this.GetType().Assembly));
+        }
+    }
+}
